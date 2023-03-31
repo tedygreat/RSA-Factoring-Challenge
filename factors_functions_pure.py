34e0cdce6339aa1e20aa6e5ce959ce5566dc63e2
tedygreat <tedygreat@gmail.com>
2023-03-31 14:32:34 -0700

    ta

--- factors_functions_pure.py
+++ factors_functions_pure.py
@@ -6,23 +6,16 @@ from time import time as timestamp
 
 
 def unix_time(function):
-    '''Return `real`, `sys` and `user` elapsed time, like UNIX's command `time`
-    You can calculate the amount of used CPU-time used by your
-    function/callable by summing `user` and `sys`. `real` is just like the wall
-    clock.
-    Note that `sys` and `user`'s resolutions are limited by the resolution of
-    the operating system's software clock (check `man 7 time` for more
-    details).
-    '''
     start_time, start_resources = timestamp(), resource_usage(RUSAGE_SELF)
     function()
     end_resources, end_time = resource_usage(RUSAGE_SELF), timestamp()
-
+    return '' 
+'''
     return "\nreal: {}\nuser: {}\nsys: {}\n".format(
         end_time - start_time,
         end_resources.ru_utime - start_resources.ru_utime,
         end_resources.ru_stime - start_resources.ru_stime)
-
+'''
 
 def trial_division(n: int) -> int:
     """
